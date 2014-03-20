74f88e2d820bf05d4e127f19bd348c07fa4335b3
Kamila Souckova <kamila@vesmir.sk>
2014-03-20 22:37:20 +0100

    tidy up stm32f0.h

--- device/stm32f0.h
+++ device/stm32f0.h
@@ -5,6 +5,8 @@
 
 /* === memory map =========================================================== */
 
+extern const word FLASH_BASE, RAM_BASE;
+
 #define PERIPH_BASE  0x40000000
 
 #define APB_PERIPH_BASE  (PERIPH_BASE)
@@ -22,19 +24,13 @@ typedef struct {
     IO uint32_t AHBENR;    // AHB peripheral clock register          offset 0x14
     IO uint32_t APB2ENR;   // APB2 peripheral clock enable register  offset 0x18
     IO uint32_t APB1ENR;   // APB1 peripheral clock enable register  offset 0x1C
-    IO uint32_t BDCR;      // Backup domain control register         offset 0x20 
+    IO uint32_t BDCR;      // Backup domain control register         offset 0x20
     IO uint32_t CSR;       // clock control & status register        offset 0x24
     IO uint32_t AHBRSTR;   // AHB peripheral reset register          offset 0x28
     IO uint32_t CFGR2;     // clock configuration register 2         offset 0x2C
     IO uint32_t CFGR3;     // clock configuration register 3         offset 0x30
     IO uint32_t CR2;       // clock control register 2               offset 0x34
 } RCC_struct;
-
-typedef enum {
-    RCC_AHBENR_GPIOA_BIT = 17,  // GPIOA clock enable bit
-    RCC_AHBENR_GPIOB_BIT = 18,  // GPIOB clock enable bit
-    RCC_AHBENR_GPIOC_BIT = 19,  // GPIOC clock enable bit
-} RCC_ahbenr_bit;
 
 #define RCC  ((RCC_struct*)(AHBPERIPH_BASE + 0x00001000))
 
@@ -43,18 +39,18 @@ typedef enum {
 typedef struct {
     IO uint32_t MODER;       // mode register                   offset 0x00
     IO uint16_t OTYPER;      // output type register            offset 0x04
-    uint16_t    RESERVED0;   // reserved
+    uint16_t    RESERVED0;
     IO uint32_t OSPEEDR;     // output speed register           offset 0x08
     IO uint32_t PUPDR;       // pull-up/pull-down register      offset 0x0C
     IO uint16_t IDR;         // input data register             offset 0x10
-    uint16_t    RESERVED1;   // reserved
+    uint16_t    RESERVED1;
     IO uint16_t ODR;         // output data register            offset 0x14
-    uint16_t    RESERVED2;   // reserved
+    uint16_t    RESERVED2;
     IO uint32_t BSRR;        // bit set/reset register          offset 0x18
     IO uint32_t LCKR;        // configuration lock register     offset 0x1C
     IO uint32_t AFR[2];      // alternate function registers    offset 0x20-0x24
     IO uint16_t BRR;         // bit reset register              offset 0x28
-    uint16_t    RESERVED3;   // reserved
+    uint16_t    RESERVED3;
 } GPIO_struct;
 
 typedef enum {
