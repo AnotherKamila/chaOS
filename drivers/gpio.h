f27ce0a0a7e76bc27019028a6a5ce6dd6f29439c
Kamila Souckova <kamila@vesmir.sk>
2014-03-20 22:33:49 +0100

    add GPIO_pin_set; change pin type to uint8_t

--- drivers/gpio.h
+++ drivers/gpio.h
@@ -6,18 +6,22 @@ inline static void GPIO_init(const Port port) {
     bit_on(RCC->AHBENR, port._GPIO_RCC_bit);  // enable clock on that port
 }
 
-inline static void GPIO_setup_pin(const Port port, int pin, GPIO_mode mode, GPIO_otype otype, GPIO_ospeed ospeed) {
+inline static void GPIO_setup_pin(const Port port, uint8_t pin, GPIO_mode mode, GPIO_otype otype, GPIO_ospeed ospeed) {
     bit_mset(port.GPIO->MODER,   0x3 << 2*pin, mode   << 2*pin);  // 2 bits per pin
     bit_mset(port.GPIO->OTYPER,  0x1 <<   pin, otype  << 2*pin);  // 1 bit per pin
     bit_mset(port.GPIO->OSPEEDR, 0x3 << 2*pin, ospeed << 2*pin);  // 2 bits per pin
 }
 
-inline static void GPIO_pin_on(const Port port, int pin) {
+// the following are atomic access (rather than read-modify-write)
+inline static void GPIO_pin_on(const Port port, uint8_t pin) {
     // TODO assert that it is set as output
     port.GPIO->BSRR = 1 << pin;
 }
-
-inline static void GPIO_pin_off(const Port port, int pin) {
+inline static void GPIO_pin_off(const Port port, uint8_t pin) {
     // TODO assert that it is set as output
     port.GPIO->BRR = 1 << pin;
+}
+inline static void GPIO_pin_set(const Port port, uint8_t pin, bool value) {
+    if (value) GPIO_pin_on(port, pin);
+    else GPIO_pin_off(port, pin);
 }
