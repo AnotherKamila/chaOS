5a009f27066d0f76cf2b7e9075f4937715b6b59d
Kamila Souckova <kamila@vesmir.sk>
2014-04-06 21:00:37 +0200

    add curly braces everywhere

--- cslibc/string/string.c
+++ cslibc/string/string.c
@@ -4,22 +4,26 @@
 
 int memcmp(const void *s1, const void *s2, register size_t n) {
     register char *p1 = (char*)s1, *p2 = (char*)s2;
-    while (n--)
-        if (*p1++ != *p2++)
+    while (n--) {
+        if (*p1++ != *p2++) {
             break;
+        }
+    }
     return n;
 }
 
 void *memcpy(void* const dest, void* const src, register const size_t n) {
     register char *ps = src, *pd = dest;
-    while ((uintptr_t)ps < (uintptr_t)src+n)
+    while ((uintptr_t)ps < (uintptr_t)src+n) {
         *pd++ = *ps++;
+    }
     return dest;
 }
 
 void *memset(void* const start, register const int f, register const size_t n) {
     register char* p = start;
-    while ((uintptr_t)p < (uintptr_t)start+n)
+    while ((uintptr_t)p < (uintptr_t)start+n) {
         *(p++) = f;
+    }
     return start;
 }
