90687563151a51f59eb7051fad62f51b86099a41
Kamila Souckova <kamila@vesmir.sk>
2014-04-06 10:21:54 +0200

    add Csucks.h -- renames some keywords

---
+++ cslibc/inc/Csucks.h
@@ -0,0 +1,17 @@
+/* Provides aliases for some types and C keywords I don't like.
+   The rest of cslibc does not use this, it's here just for convenience. */
+
+#ifndef _CSUCKS_H
+#define _CSUCKS_H
+
+
+#include "stdbool.h"  // everybody wants this anyway
+
+#define intern    static  // for use in file scope
+#define common    static  // for use in function scope
+#define readonly  const   // it's not constant, it's just read-only!
+
+#define byte  char  // to be used with signed/unsigned (that's why it isn't a typedef)
+
+
+#endif
