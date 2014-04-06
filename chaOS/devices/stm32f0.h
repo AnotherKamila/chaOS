5660d6d60383d0ba8f29a61497a292e136bd3ac5
Kamila Souckova <kamila@vesmir.sk>
2014-04-06 10:23:35 +0200

    stm32f0.h: add registers info

--- chaOS/devices/stm32f0.h
+++ chaOS/devices/stm32f0.h
@@ -6,6 +6,10 @@
 
 ////// CPU internals //////////////////////////////////////////////////////////////////////////////
 
+// all general-purpose registers, plus sp, lr, pc
+#define REGISTERS_SET  {r0-r15}
+#define NUM_REGISTERS  16
+
 // see http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0204f/Cihfddaf.html
 #define _ARM_BLX_SHIT
 
