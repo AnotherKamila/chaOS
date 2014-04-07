2f88dc2ee9f6d577d17173260523a0087d82a0d7
Kamila Souckova <kamila@vesmir.sk>
2014-04-07 13:15:07 +0200

    move code from .h to .c (where it is not pointless)

--- README.md
+++ README.md
@@ -76,7 +76,6 @@ No Cortex-M MCUs have an MMU. The M4 is the only one with an MPU (so yes, my cur
 
 ### Immediate TODO:
 
- - move code from .h into .c + change `static` to `intern` now that I have it
  - malloc
  - throw gpio.h into a black hole
  - tidy up that scary WIP branch, merge what you can, publish it
--- chaOS/binfmt/elf.h
+++ chaOS/binfmt/elf.h
@@ -2,11 +2,8 @@
 #define _BINFMT_ELF_H
 
 
-#include "string.h"
 #include "devices/core.h"
 #include "binfmt/common.h"
-
-#define TO_ADDR  (RAM_BASE + 0x200)  // TODO
 
 #define E_NOT_ELF_X       1
 #define E_NOT_COMPATIBLE  2
@@ -74,65 +71,7 @@ enum sh_flags {
     SHF_EXEC  = 0x4,
 };
 
-static bool is_elf_x(ELF32_hdr *hdr) {
-    return hdr->e_ident[0] == EID0 &&
-           hdr->e_ident[1] == 'E'  &&
-           hdr->e_ident[2] == 'L'  &&
-           hdr->e_ident[3] == 'F'  &&
-           (hdr->e_type == ET_EXEC || hdr->e_type == ET_DYN);
-}
-static bool is_compatible(ELF32_hdr *hdr) {
-    return hdr->e_ident[4] == EC_32      &&
-           hdr->e_version  == EV_CURRENT &&
-           hdr->e_machine  == EM_ARM;
-}
-
-// TODO load from something file-like
-static int load_elf(const program_img *prg, exec_img *res) {
-    uintptr_t from_addr = (uintptr_t)prg->img;
-    uintptr_t to_addr = TO_ADDR;  // TODO allocate memory instead of this :D
-    // assuming my binary starts at 0x0
-    ELF32_hdr *hdr = (ELF32_hdr*)prg->img;
-    if (!is_elf_x(hdr)) {
-        return E_NOT_ELF_X;
-    }
-    if (!is_compatible(hdr)) {
-        return E_NOT_COMPATIBLE;
-    }
-
-    ELF32_shdr *sections = (ELF32_shdr*)((uintptr_t)hdr + hdr->e_shoff);
-    char *strings = (char*)(from_addr + sections[hdr->e_shstrndx].sh_off);
-
-    // prepare .bss, relocate GOT, load .text and .data
-    for (int sec_idx = 0; sec_idx < hdr->e_shnum; ++sec_idx) {
-        ELF32_shdr *shdr = &sections[sec_idx];
-        if (shdr->sh_flags & SHF_ALLOC && shdr->sh_size) {  // non-empty allocatable sections
-            uintptr_t daddr = to_addr + shdr->sh_addr;  // poor man's relocation :D
-            char *name = strings + shdr->sh_name;
-            if (memcmp(name, ".got", 4) == 0) {  // handle relocation -- copy adding offset
-                // TODO assert that shdr->sh_entsize is 4 bytes
-                uint32_t *src  = (uint32_t*)(from_addr + shdr->sh_off);
-                uint32_t *dest = (uint32_t*)daddr;
-                while ((uintptr_t)dest < (uint32_t)(daddr + shdr->sh_size)) {
-                    *dest++ = to_addr + *src++;  // to_addr is also the offset (as start is at 0x0)
-                }
-            }
-            else {
-                switch (shdr->sh_type) {
-                    case SHT_NOBITS:  // .bss
-                        memset((void*)daddr, 0, shdr->sh_size);
-                        break;
-                    case SHT_PROGBITS:  // .data, .rodata, .text (should use FLAGS to set rwx perms)
-                        memcpy((void*)daddr, (void*)(from_addr + shdr->sh_off), shdr->sh_size);
-                        break;
-                }
-            }
-        }
-    }
-
-    res->entry = _tofunc(to_addr + hdr->e_entry);  // traces of my poor man's relocation too
-    return 0;
-}
+int load_elf(const program_img *prg, exec_img *res);
 
 
 #endif
