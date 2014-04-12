bfe10bf8acd8092ccc533fcaf270b5ab687aaf6f
Kamila Souckova <kamila@vesmir.sk>
2014-04-12 18:19:43 +0200

    change interrupts infrastucture

---
+++ chaOS/devices/stm32f0/interrupts.h
@@ -0,0 +1,18 @@
+/* declares interrupt handlers (can be overridden by declaring a (strong) function with the same
+   name) */
+
+#include "devices/core.h"
+
+#define ISR_NOIMPL(name)  \
+        void name(void) __attribute__((weak, alias("default_interrupt_handler")))
+
+#define ISR(name)  \
+        void name(void) __attribute__((weak)));                                                   \
+        void name(void)  // expects to see { /* implementation */ }
+
+#define EXC_NOIMPL(name)  \
+        void name(void) __attribute__((weak, alias("default_exception_handler")))
+
+// must be defined somewhere, e.g. in interrupts.c
+void default_exception_handler(void) __attribute__((weak));
+void default_interrupt_handler(void) __attribute__((weak));
