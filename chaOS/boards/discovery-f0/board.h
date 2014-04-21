11542ca428cb85466cfc19cba55b12ae96c2bca6
Kamila Souckova <kamila@vesmir.sk>
2014-04-22 00:47:18 +0200

    create subdirs for kernel subsystems

--- chaOS/boards/discovery-f0/board.h
+++ chaOS/boards/discovery-f0/board.h
@@ -4,6 +4,9 @@
 
 #include "drivers/gpio.h"
 
+//                                               PORTA  PORTB  PORTC  PORTD  PORTE  PORTF
+static const bool port_connected[PORT_MAX+1] = {  true,  true,  true, false, false, false };
+
 #define LED_PORT  PORTC
 #define LED1      PIN(8)
 #define LED2      PIN(9)
