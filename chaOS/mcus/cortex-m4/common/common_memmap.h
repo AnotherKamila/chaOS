15e758bc5cc4f3542981efc654f98a494405a655
Adam Dej <dejko.a@gmail.com>
2014-04-25 20:17:12 +0200

    CortexM4: Add common memory map

---
+++ chaOS/mcus/cortex-m4/common/common_memmap.h
@@ -0,0 +1,14 @@
+/* defines CPU family common memory map and declares peripherals */
+
+#ifndef MCUS_CORTEXM4_COMMON_MEMMAP_H
+#define MCUS_CORTEXM4_COMMON_MEMMAP_H
+
+
+#include "common_peripherals.h"
+
+/* --- Cortex M4 peripherals base address --------------------------------------------------------*/
+
+#define CORE_PERIPH_BASE    0xE0000000
+
+
+#endif
---
+++ chaOS/mcus/cortex-m4/common/common_peripherals.h
@@ -0,0 +1,10 @@
+/* defines common MCU family peripherals and related things like flags */
+
+#ifndef MCUS_CORTEXM4_COMMON_PERIPHERALS_H
+#define MCUS_CORTEXM4_COMMON_PERIPHERALS_H
+
+
+#include "core.h"
+
+
+#endif
