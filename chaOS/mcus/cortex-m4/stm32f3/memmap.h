65c39c49157195a2c4b5e366d4691143e234c62b
Adam Dej <dejko.a@gmail.com>
2014-04-25 20:39:58 +0200

    STM32F3: Add memory map

---
+++ chaOS/mcus/cortex-m4/stm32f3/memmap.h
@@ -0,0 +1,38 @@
+/* defines memory map and declares peripherals */
+
+#ifndef MCUS_CORTEXM4_STM32F3_MEMMAP_H
+#define MCUS_CORTEXM4_STM32F3_MEMMAP_H
+
+
+#include "peripherals.h"
+
+/* --- Peripherals base address ------------------------------------------------------------------*/
+
+#define PERIPH_BASE             0x40000000
+
+/* --- Buses base addresses ----------------------------------------------------------------------*/
+
+#define APB1_PERIPH_BASE        (PERIPH_BASE)
+#define APB2_PERIPH_BASE        (PERIPH_BASE + 0x00010000)
+#define AHB1_PERIPH_BASE        (PERIPH_BASE + 0x00020000)
+#define AHB2_PERIPH_BASE        (PERIPH_BASE + 0x08000000)
+#define AHB3_PERIPH_BASE        (PERIPH_BASE + 0x10000000)
+
+/* --- Peripheral address offsets ----------------------------------------------------------------*/
+
+#define GPIO_AHB2_OFFSET_PORTA  0x00000000
+#define GPIO_AHB2_OFFSET_PORTB  0x00000400
+#define GPIO_AHB2_OFFSET_PORTC  0x00000800
+#define GPIO_AHB2_OFFSET_PORTD  0x00000C00
+#define GPIO_AHB2_OFFSET_PORTE  0x00001000
+#define GPIO_AHB2_OFFSET_PORTF  0x00001400
+
+#define RCC_AHB1_OFFSET         0x00001000
+
+/* --- Peripherals declaration -------------------------------------------------------------------*/
+
+extern GPIO_struct * const GPIO[];
+extern RCC_struct  * const RCC;
+
+
+#endif
