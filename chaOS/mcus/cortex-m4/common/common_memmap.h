d1ae0106efbd82a043fce240c6c1e478142b6c8a
Adam Dej <dejko.a@gmail.com>
2014-04-26 12:33:02 +0200

    CortexM4: Add SysTick to memmap

--- chaOS/mcus/cortex-m4/common/common_memmap.h
+++ chaOS/mcus/cortex-m4/common/common_memmap.h
@@ -10,5 +10,13 @@
 
 #define CORE_PERIPH_BASE    0xE0000000
 
+/* --- Cortex M4 peripherals addresses offsets ---------------------------------------------------*/
+
+#define CORE_PBASE_SYSTICK_OFFSET   0x0000E010
+
+/* --- Cortex M0 peripherals declaration ---------------------------------------------------------*/
+
+extern STK_struct  * const STK;
+
 
 #endif
--- chaOS/mcus/cortex-m4/common/common_peripherals.h
+++ chaOS/mcus/cortex-m4/common/common_peripherals.h
@@ -6,5 +6,26 @@
 
 #include "core.h"
 
+/* --- SysTick timer -----------------------------------------------------------------------------*/
+
+typedef struct {
+    _IO uint32_t CSR;       // Control and status register                          offset 0x00
+    _IO uint32_t RVR;       // Reload value register                                offset 0x04
+    _IO uint32_t CVR;       // Current value register                               offset 0x08
+    _O  uint32_t CALIB;     // Calibration value register                           offset 0x0C
+} STK_struct;
+
+enum STK_CSR_bits {
+    CSR_ENABLE      = 0,
+    CSR_TICKINT     = 1,
+    CSR_CLKSOURCE   = 2,
+    CSR_COUNTFLAG   = 16,
+};
+
+enum STK_CALIB_bits {
+    CALIB_NOREF       = 31,
+    CALIB_SKEW        = 30,
+};
+
 
 #endif
