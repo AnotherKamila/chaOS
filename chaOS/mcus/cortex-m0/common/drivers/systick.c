b1e4e188b5d2f3ab54e19b136e27c283c7d8a3f9
Adam Dej <dejko.a@gmail.com>
2014-05-05 14:48:51 +0200

    CortexM0: Change HAS_SYSTICK checking mechanism

--- chaOS/mcus/cortex-m0/common/drivers/systick.c
+++ chaOS/mcus/cortex-m0/common/drivers/systick.c
@@ -6,6 +6,58 @@
 //This header should define HAS_SYSTICK if CPU we are compiling for has it
 #include "cpu.h"
 
+#if !HAS_SYSTICK
+//Systick is missing, provide only stub functions returning SYSTICK_FEATURE_MISSING
+
+int systick_config(const enum SysTick_clocksrc clock __attribute__((unused)),
+                   const bool do_interrupt __attribute__((unused))) {
+    return SYSTICK_FEATURE_MISSING;
+}
+
+void systick_disable_interrupt() {
+    ;
+}
+
+bool systick_is_interrupt_enabled() {
+    return false; // If systick is missing it's interrupt cannot be enabled
+}
+
+int systick_enable() {
+    return SYSTICK_FEATURE_MISSING;
+}
+
+int systick_disable() {
+    return SYSTICK_FEATURE_MISSING;
+}
+
+uint32_t systick_get_status() {
+    return SYSTICK_FLAG_DISABLED; // If systick is missing it cannot be enabled
+}
+
+int systick_read(uint32_t *value __attribute__((unused))) {
+    return SYSTICK_FEATURE_MISSING;
+}
+
+int systick_write(uint32_t value __attribute__((unused))) {
+    return SYSTICK_FEATURE_MISSING;
+}
+
+int systick_set_top(uint32_t value __attribute__((unused))) {
+    return SYSTICK_FEATURE_MISSING;
+}
+
+int systick_get_top(uint32_t *value __attribute__((unused))) {
+    return SYSTICK_FEATURE_MISSING;
+}
+
+int systick_get_calibration(uint32_t *value __attribute__((unused))) {
+    return SYSTICK_FEATURE_MISSING;
+}
+
+#else
+
+//Systick is present, we can implement the driver now
+
 /*
  * We must never read the CSR register (except for reading COUNTFLAG flag) because COUNTFLAG
  * is reset after every read. This is its local copy for read-modify-write
@@ -13,10 +65,6 @@
 static uint32_t csr_local;
 
 int systick_config(const enum SysTick_clocksrc clock, const bool do_interrupt) {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FEATURE_MISSING;
-    #else
-
     csr_local = 0;
 
     //Systick current and reload value are undefined at reset, this is the recommended init order:
@@ -30,48 +78,30 @@ int systick_config(const enum SysTick_clocksrc clock, const bool do_interrupt) {
     STK->CSR = csr_local;
 
     return SYSTICK_SUCCESS;
-    #endif
 }
 
 void systick_disable_interrupt() {
-    #ifdef HAS_SYSTICK
     bit_moff(csr_local, (1 << CSR_TICKINT));
     STK->CSR = csr_local;
-    #endif
 }
 
 bool systick_is_interrupt_enabled() {
-    #ifdef HAS_SYSTICK
     return csr_local & (1 << CSR_TICKINT);
-    #else
-    return false
-    #endif
 }
 
 int systick_enable() {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FEATURE_MISSING;
-    #else
     bit_mon(csr_local, (1 << CSR_ENABLE));
     STK->CSR = csr_local;
     return SYSTICK_SUCCESS;
-    #endif
 }
 
 int systick_disable() {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FEATURE_MISSING;
-    #else
     bit_moff(csr_local, (1 << CSR_ENABLE));
     STK->CSR = csr_local;
     return SYSTICK_SUCCESS;
-    #endif
 }
 
 uint32_t systick_get_status() {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FLAG_DISABLED;
-    #else
     uint32_t flags = 0;
     if (csr_local & (1 << CSR_ENABLE)) flags |= SYSTICK_FLAG_ENABLED;
     if (STK->CALIB & (1 << CALIB_NOREF)) flags |= SYSTICK_FLAG_NOREFC;
@@ -79,57 +109,37 @@ uint32_t systick_get_status() {
     // After this read completes the COUNTFLAG resets
     if (STK->CSR & (1 << CSR_COUNTFLAG)) flags |= SYSTICK_FLAG_WASZERO;
     return flags;
-    #endif
 }
 
 int systick_read(uint32_t *value) {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FEATURE_MISSING;
-    #else
     *value = STK->CVR;
     return SYSTICK_SUCCESS;
-    #endif
 }
 
 int systick_write(uint32_t value) {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FEATURE_MISSING;
-    #else
     if (value & 0xFF000000) {
         //Highest 8 bits are reserved and cannot be written to
         return SYSTICK_VAL_OUT_OF_RANGE;
     }
     STK->CVR = value;
     return SYSTICK_SUCCESS;
-    #endif
 }
 
 int systick_set_top(uint32_t value) {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FEATURE_MISSING;
-    #else
     if (value & 0xFF000000) {
         //Highest 8 bits are reserved and cannot be written to
         return SYSTICK_VAL_OUT_OF_RANGE;
     }
     STK->RVR = value;
     return SYSTICK_SUCCESS;
-    #endif
 }
 
 int systick_get_top(uint32_t *value) {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FEATURE_MISSING;
-    #else
     *value = STK->RVR;
     return SYSTICK_SUCCESS;
-    #endif
 }
 
 int systick_get_calibration(uint32_t *value) {
-    #ifndef HAS_SYSTICK
-    return SYSTICK_FEATURE_MISSING;
-    #else
     //Mask out reserved bits
     if (STK->CALIB & 0x00FFFFFF) {
         //Calibration value is not zero, therefore is known
@@ -139,5 +149,6 @@ int systick_get_calibration(uint32_t *value) {
         *value = 0;
         return SYSTICK_CALIB_UNKNOWN;
     }
-    #endif
 }
+
+#endif
